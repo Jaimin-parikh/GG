eaa68bfd63ac6d5f82e039d7279a5ae2ad3e2d63
Jaimin-parikh <jaiminparikh12@gmail.com>
2024-02-09 12:37:12 +0530

    Hollow Diamond Pattern

---
+++ FEB2024/09/Hollow_diamond.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br><br><h2>
+    <form action="Hollow_diamond.php" method="POST">
+
+    INPUT : <input type="text" name = "input">
+    <input type="submit" value="Submit">
+    </form></h2>
+</body>
+</html>
+
+
+<?php
+
+$n = $_POST['input'];
+echo "<h2>";
+$k = (2*$n-1);
+for($i=1; $i<=$n; $i++){
+    echo "<br>";
+    for($j=1;$j<= $k;$j++){
+        if(($i+$j) == ($n+1 )|| ($n-$i)== ($k-$j)){
+            echo " *";
+        }
+        else{
+            echo "&nbsp&nbsp ";
+        }
+    }
+}
+for($i=2; $i<=$n; $i++){
+    echo "<br>";
+    for($j=1;$j<= $k;$j++){
+        if($i == $j || ($i + $j) == 2*$n){
+            echo " *";
+        }
+        else{
+            echo "&nbsp&nbsp ";
+        }
+    }
+}
+echo "</h2>";
+
+?>
